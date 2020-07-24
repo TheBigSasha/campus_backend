09f2b1a2b99416b1161f9c6267791167effd4c36
thebigsasha <sashathethird@gmail.com>
2020-07-23 22:03:43 -0400

    Some misc changes

--- ZUMOAPPNAMEService/DataObjects/TodoItem.cs
+++ ZUMOAPPNAMEService/DataObjects/TodoItem.cs
@@ -3,11 +3,12 @@
 namespace ZUMOAPPNAMEService.DataObjects
 {
     public class TodoItem : EntityData
-    { 
-        public string Name { get; set; }
-   
+    {
         public string Text { get; set; }
 
         public bool Complete { get; set; }
+
+        public string Name { get; set; }
+
     }
 }
