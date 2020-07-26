a6fd9f4aec282787dc6038fa9cfa230d02c1dff0
thebigsasha <sashathethird@gmail.com>
2020-07-26 00:45:01 -0400

    Add todo

--- ZUMOAPPNAMEService/DataObjects/TodoItem.cs
+++ ZUMOAPPNAMEService/DataObjects/TodoItem.cs
@@ -11,5 +11,7 @@ namespace ZUMOAPPNAMEService.DataObjects
 
         public bool Complete { get; set; }
 
+        //TODO: Add varbinary or blob for profile pic, date for bday, xml for[university, major, year], [xml for social media: instagram, facebook, snapchat, linkedin, email], xml for class list
+
     }
 }
